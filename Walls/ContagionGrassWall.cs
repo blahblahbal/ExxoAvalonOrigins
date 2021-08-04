f719504a1a75722476232f974378ef4fb29832c8
Blasfah <Geometrydashgamer555@gmail.com>
2021-08-05 00:28:29 +0200

    Stuff

---
+++ Walls/ContagionGrassWall.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Terraria;using Terraria.ModLoader;using Terraria.ID;namespace ExxoAvalonOrigins.Walls{
+    public class ContagionGrassWall : ModWall
+    {
+        public override void SetDefaults()
+        {            AddMapEntry(new Color(82, 91, 69));
+            soundType = SoundID.Grass;
+            soundStyle = 1;
+            WallID.Sets.Conversion.Grass[base.Type] = true;
+        }
+    }}
