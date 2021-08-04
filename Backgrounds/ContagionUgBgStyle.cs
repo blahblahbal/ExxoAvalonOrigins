e87b50c46f99b42c21c470069a93bc27a00263a6
Blasfah <Geometrydashgamer555@gmail.com>
2021-08-04 05:55:27 +0200

    Contagion wall thing stuff

---
+++ Backgrounds/ContagionUgBgStyle.cs
@@ -0,0 +1,20 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ExxoAvalonOrigins.Backgrounds
+{
+	public class ContagionUgBgStyle : ModUgBgStyle
+	{
+		public override bool ChooseBgStyle() => Main.LocalPlayer.GetModPlayer<ExxoAvalonOriginsModPlayer>().zoneBooger;
+
+		public override void FillTextureArray(int[] textureSlots)
+		{
+			textureSlots[0] = mod.GetBackgroundSlot("Backgrounds/ContagionUG0");
+			textureSlots[1] = mod.GetBackgroundSlot("Backgrounds/ContagionUG1");
+			textureSlots[2] = mod.GetBackgroundSlot("Backgrounds/ContagionUG2");
+			textureSlots[3] = mod.GetBackgroundSlot("Backgrounds/ContagionUG3");
+			textureSlots[4] = mod.GetBackgroundSlot("Backgrounds/ContagionUG5");
+			textureSlots[5] = mod.GetBackgroundSlot("Backgrounds/ContagionUG4");
+		}
+	}
+}
