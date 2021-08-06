483c5af6ef7f53210dcf4a681995b08726eb0a1a
TerrorPenguin <41289369+TerrorPenguin@users.noreply.github.com>
2021-08-07 09:44:35 +1000

    Baccilite & Ectoplasm Lighting

--- Tiles/BacciliteOre.cs
+++ Tiles/BacciliteOre.cs
@@ -21,11 +21,20 @@ namespace ExxoAvalonOrigins.Tiles
 			Main.tileMergeDirt[Type] = true;
 			Main.tileShine[Type] = 1150;
             Main.tileSpelunker[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileValue[Type] = 315;
+			Main.tileLighted[Type] = true;
 			drop = ModContent.ItemType<Items.BacciliteOre>();
             soundType = SoundID.Tink;
             soundStyle = 1;
             dustType = 44;
             minPick = 55;
+		}
+		
+		public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
+		{
+			r = 0.18f;
+			g = 0.25f;
 		}
 	}
 }
--- Tiles/Ectoplasm.cs
+++ Tiles/Ectoplasm.cs
@@ -1,8 +1,8 @@
-using Microsoft.Xna.Framework;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Terraria;using Terraria.ModLoader;using Terraria.ID;using Terraria.ObjectData;namespace ExxoAvalonOrigins.Tiles{	public class Ectoplasm : ModTile	{		public override void SetDefaults()		{			AddMapEntry(new Color(27, 194, 254));			Main.tileSolid[Type] = true;			Main.tileLargeFrames[Type] = 1;			Main.tileBlockLight[Type] = true;			drop = ItemID.Ectoplasm;            soundType = SoundID.NPCHit;            soundStyle = 1;
+using Microsoft.Xna.Framework;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Terraria;using Terraria.ModLoader;using Terraria.ID;using Terraria.ObjectData;namespace ExxoAvalonOrigins.Tiles{	public class Ectoplasm : ModTile	{		public override void SetDefaults()		{			AddMapEntry(new Color(27, 194, 254));			Main.tileSolid[Type] = true;			Main.tileLargeFrames[Type] = 1;			Main.tileBlockLight[Type] = true;			Main.tileLighted[Type] = true;			drop = ItemID.Ectoplasm;            soundType = SoundID.NPCHit;            soundStyle = 1;
             dustType = 156;
         }        public override bool KillSound(int i, int j)
         {
             if (Main.rand.Next(10) == 0) Main.PlaySound(4, i * 16, j * 16, 6);
             return true;
-        }
+        }				public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)		{			r = 0.16f;			g = 0.2f;			b = 0.3f;		}
     }}
