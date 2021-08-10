226df86975628e6eaff33bad881860035b174d51
Brian Hansen <jabawakijadehawk@gmail.com>
2021-08-09 20:09:11 -0700

    Freezethrower dust

---
+++ Dusts/FreezethrowerDust.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace ExxoAvalonOrigins.Dusts
+{
+    public class FreezethrowerDust : ModDust
+    {
+        public override bool Update(Dust dust)
+        {
+            Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), 135 / 255, 140 / 255, 1);
+            return true;
+        }
+    }
+}
