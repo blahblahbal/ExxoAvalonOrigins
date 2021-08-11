bf91f274947fcc37c92a3f21e8e41190bba6be09
TerrorPenguin <41289369+TerrorPenguin@users.noreply.github.com>
2021-08-11 20:05:07 +1000

    Dust edits
    - made the light fade out slowly
    (if there's an easier or better way to do this then please edit it)
    
    - adjusted colour of light
    - made freezethrower dust bright
    - triton flame dust now emits light (yellow & blue)

--- Dusts/FreezethrowerDust.cs
+++ Dusts/FreezethrowerDust.cs
@@ -1,4 +1,5 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace ExxoAvalonOrigins.Dusts
@@ -7,8 +8,14 @@ namespace ExxoAvalonOrigins.Dusts
     {
         public override bool Update(Dust dust)
         {
-            Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), 135 / 255, 140 / 255, 1);
+			var lightFade = (dust.scale > 1 ? 1 : dust.scale);
+            Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), ((float)67 / 255 * lightFade), ((float)70 / 255 * lightFade), 1 * lightFade);
             return true;
+        }
+		
+        public override Color? GetAlpha(Dust dust, Color lightColor)
+        {
+            return new Color(135, 140, 255, 100);
         }
     }
 }
