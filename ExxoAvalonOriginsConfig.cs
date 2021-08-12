ec04d1563b67f0fc032859a22b83ae2c2548fe2a
John Boyer <bojopro@gmail.com>
2021-08-12 17:10:07 +1000

    Renamed WorldList to UIMenuContagion, Added config with hidden from player data to cache the contagion status of worlds

---
+++ ExxoAvalonOriginsConfig.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria.ModLoader;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ModLoader.IO;
+using System.IO;
+using Terraria.Utilities;
+using System.Reflection;
+using Terraria.IO;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using Terraria;
+using Mono.Cecil;
+using Microsoft.Xna.Framework;
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using Terraria.ID;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.Config.UI;
+using Terraria.UI;
+using Newtonsoft.Json;
+
+namespace ExxoAvalonOrigins
+{
+	public class ExxoAvalonOriginsConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+
+		// Key value for each twld path and if contagion or not
+		[DefaultListValue(false)]
+		[JsonProperty]
+		private Dictionary<string, bool> worldContagionStatus = new Dictionary<string, bool>();
+
+		// Methods to avoid public variable getting picked up by serialiser
+		public Dictionary<string, bool> GetWorldContagionStatus() { return worldContagionStatus; }
+		public void SetWorldContagionStatus(Dictionary<string, bool> newDict) { worldContagionStatus = newDict; }
+
+		public static void Save(ModConfig config)
+		{
+			Directory.CreateDirectory(ConfigManager.ModConfigPath);
+			string filename = config.mod.Name + "_" + config.Name + ".json";
+			string path = Path.Combine(ConfigManager.ModConfigPath, filename);
+			string json = JsonConvert.SerializeObject((object)config, ConfigManager.serializerSettings);
+			File.WriteAllText(path, json);
+		}
+	}
+}
--- Hooks/UIMenuContagion.cs
+++ Hooks/UIMenuContagion.cs
@@ -20,58 +20,7 @@ using Microsoft.Xna.Framework;
 
 namespace ExxoAvalonOrigins.Hooks
 {
-	public class WorldList
-	{
-		public static Texture2D OnGetIcon(On.Terraria.GameContent.UI.Elements.UIWorldListItem.orig_GetIcon orig, UIWorldListItem self)
-		{
-			WorldFileData data = self.GetFieldValue<WorldFileData>("_data");
-			var path = Path.ChangeExtension(data.Path, ".twld");
-
-			if (!FileUtilities.Exists(path, data.IsCloudSave))
-			{
-				return orig(self);
-			}
-
-			//Stream stream = (data.IsCloudSave ? ((Stream)new MemoryStream(Terraria.Social.SocialAPI.Cloud.Read(path))) : ((Stream)new FileStream(path, FileMode.Open)));
-			byte[] buf = FileUtilities.ReadAllBytes(path, data.IsCloudSave);
-			if (buf[0] != 31 || buf[1] != 139)
-			{
-				return orig(self);
-			}
-
-			bool contagion = false;
-			var stream = new MemoryStream(buf);
-			var tag = TagIO.FromStream(stream);
-
-			if (tag.ContainsKey("modData"))
-			{
-				foreach (TagCompound modDataTag in tag.GetList<TagCompound>("modData").Skip(2))
-				{
-					if (modDataTag.Get<string>("mod") == ExxoAvalonOrigins.mod.Name)
-                    {
-						TagCompound dataTag = modDataTag.Get<TagCompound>("data");
-						contagion = dataTag.Get<bool>("ExxoAvalonOrigins:Contagion");
-						break;
-					}
-				}
-			}
-
-			if (contagion)
-			{
-				if (data.IsHardMode)
-                {
-					return ExxoAvalonOrigins.mod.GetTexture("Sprites/IconHallowContagion");
-				}
-				else
-                {
-					return ExxoAvalonOrigins.mod.GetTexture("Sprites/IconContagion");
-				}
-			}
-			else
-			{
-				return orig(self);
-			}
-		}
+	public class UIMenuContagion {
 		public static void ILDrawSelf(ILContext il)
         {
 			var c = new ILCursor(il);
@@ -117,7 +66,69 @@ namespace ExxoAvalonOrigins.Hooks
                 }
 			});
 		}
-		
+
+		public static Texture2D OnGetIcon(On.Terraria.GameContent.UI.Elements.UIWorldListItem.orig_GetIcon orig, UIWorldListItem self)
+		{
+			WorldFileData data = self.GetFieldValue<WorldFileData>("_data");
+			var path = Path.ChangeExtension(data.Path, ".twld");
+
+			ExxoAvalonOriginsConfig config = ModContent.GetInstance<ExxoAvalonOriginsConfig>();
+			Dictionary<string, bool> newDict = config.GetWorldContagionStatus();
+
+			if (!config.GetWorldContagionStatus().ContainsKey(path))
+			{
+				if (!FileUtilities.Exists(path, data.IsCloudSave))
+				{
+					return orig(self);
+				}
+
+				//Stream stream = (data.IsCloudSave ? ((Stream)new MemoryStream(Terraria.Social.SocialAPI.Cloud.Read(path))) : ((Stream)new FileStream(path, FileMode.Open)));
+				byte[] buf = FileUtilities.ReadAllBytes(path, data.IsCloudSave);
+				if (buf[0] != 31 || buf[1] != 139)
+				{
+					return orig(self);
+				}
+
+				var stream = new MemoryStream(buf);
+				var tag = TagIO.FromStream(stream);
+
+				if (tag.ContainsKey("modData"))
+				{
+					foreach (TagCompound modDataTag in tag.GetList<TagCompound>("modData").Skip(2))
+					{
+						if (modDataTag.Get<string>("mod") == ExxoAvalonOrigins.mod.Name)
+						{
+							TagCompound dataTag = modDataTag.Get<TagCompound>("data");
+							newDict[path] = dataTag.Get<bool>("ExxoAvalonOrigins:Contagion");
+							break;
+						}
+					}
+					if (!config.GetWorldContagionStatus().ContainsKey(path))
+					{
+						newDict[path] = false;
+					}
+					config.SetWorldContagionStatus(newDict);
+					ExxoAvalonOriginsConfig.Save(config);
+				}
+			}
+
+			if (config.GetWorldContagionStatus().ContainsKey(path) && config.GetWorldContagionStatus()[path])
+			{
+				if (data.IsHardMode)
+				{
+					return ExxoAvalonOrigins.mod.GetTexture("Sprites/IconHallowContagion");
+				}
+				else
+				{
+					return ExxoAvalonOrigins.mod.GetTexture("Sprites/IconContagion");
+				}
+			}
+			else
+			{
+				return orig(self);
+			}
+		}
+
 	}
 	public static class ReflectionExtensions
 	{
