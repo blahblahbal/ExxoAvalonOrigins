e9ef71fbbfa534fa6b30f713adf0c5892a38327f
Brian Hansen <jabawakijadehawk@gmail.com>
2021-08-13 10:48:51 -0700

    Fail tropics lol

---
+++ Tiles/TropicalGrass.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Terraria;using Terraria.ModLoader;using Terraria.ID;using Terraria.ObjectData;namespace ExxoAvalonOrigins.Tiles{	public class TropicalGrass : ModTile	{		public override void SetDefaults()		{			AddMapEntry(new Color(56, 215, 29));            SetModTree(new TropicalTree());
+            Main.tileSolid[Type] = true;			Main.tileBrick[Type] = true;			Main.tileBlockLight[Type] = true;            Main.tileBlendAll[Type] = true;            Main.tileMergeDirt[Type] = true;
+            // do drop		}        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (fail && !effectOnly)
+            {
+                Main.tile[i, j].type = (ushort)ModContent.TileType<TropicalMud>();
+                WorldGen.SquareTileFrame(i, j);
+            }
+        }
+        public override int SaplingGrowthType(ref int style)
+        {
+            style = 0;
+            return ModContent.TileType<TropicalSapling>();
+        }
+    }}
---
+++ Walls/TropicalMudWall.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Xna.Framework;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Terraria;using Terraria.ModLoader;using Terraria.ID;namespace ExxoAvalonOrigins.Walls{
+    public class TropicalMudWall : ModWall
+    {
+        public override void SetDefaults()
+        {            AddMapEntry(new Color(100, 28, 0));
+        }
+    }}
