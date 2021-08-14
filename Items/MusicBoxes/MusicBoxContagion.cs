7a0a0e5eb8b10831efd1685e7422fb9c78b1ebb8
Brian Hansen <jabawakijadehawk@gmail.com>
2021-08-14 06:36:30 -0700

    Add music boxes

---
+++ Items/MusicBoxes/MusicBoxContagion.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using Terraria;using Terraria.ModLoader;using Terraria.ID;namespace ExxoAvalonOrigins.Items.MusicBoxes{
+    class MusicBoxContagion : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Music Box (Contagion)");
+        }
+
+        public override void SetDefaults()
+        {
+            Rectangle dims = ExxoAvalonOrigins.getDims("Items/MusicBoxContagion");
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<Tiles.MusicBoxes>();
+            item.placeStyle = 0;
+            item.rare = 4;
+            item.width = dims.Width;
+            item.useTime = 10;
+            item.value = Item.sellPrice(0, 2, 0, 0);
+            item.useStyle = 1;
+            item.useAnimation = 15;
+            item.height = dims.Height;
+        }
+    }}
---
+++ Tiles/MusicBoxes.cs
@@ -0,0 +1,89 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ExxoAvalonOrigins.Tiles
+{
+    internal class MusicBoxes : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.addTile(Type);
+            disableSmartCursor = true;
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Music Box");
+            AddMapEntry(new Color(200, 200, 200), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            int item = 0;
+            switch (frameY / 36)
+            {
+                case 0:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxContagion>();
+                    break;
+                case 1:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxBacteriumPrime>();
+                    break;
+                case 2:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxArmageddonSlime>();
+                    break;
+                case 3:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxDesertBeak>();
+                    break;
+                case 4:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxUndergroundContagion>();
+                    break;
+                case 5:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxTropics>();
+                    break;
+                case 6:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxPhantasm>();
+                    break;
+            }
+            Item.NewItem(i * 16, j * 16, 16, 48, item);
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            int item = 0;
+            switch (Main.tile[i, j].frameY / 36)
+            {
+                case 0:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxContagion>();
+                    break;
+                case 1:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxBacteriumPrime>();
+                    break;
+                case 2:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxArmageddonSlime>();
+                    break;
+                case 3:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxDesertBeak>();
+                    break;
+                case 4:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxUndergroundContagion>();
+                    break;
+                case 5:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxTropics>();
+                    break;
+                case 6:
+                    item = ModContent.ItemType<Items.MusicBoxes.MusicBoxPhantasm>();
+                    break;
+            }
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.showItemIcon = true;
+            player.showItemIcon2 = item;
+        }
+    }
+}
