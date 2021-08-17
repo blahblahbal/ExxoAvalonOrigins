83b07f450b277c00c0521e81d5c7e13e2222aae1
John Boyer <bojopro@gmail.com>
2021-08-18 01:53:25 +1000

    Menu selection overhaul

---
+++ UI/GenericSelectionMenu.cs
@@ -0,0 +1,100 @@
+﻿using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.GameInput;
+using Terraria.Localization;
+using Terraria.UI;
+using Terraria.UI.Gamepad;
+
+namespace ExxoAvalonOrigins.UI
+{
+	public class GenericSelectionMenu : UIState
+	{
+		private UIList optionList;
+		private UITextPanel<LocalizedText> backPanel;
+		private UIPanel containerPanel;
+
+		private bool skipDraw;
+
+		public GenericSelectionMenu(string title, UIList list, UIElement.MouseEvent cancelAction)
+        {
+			UIElement uIElement = new UIElement();
+			uIElement.Width.Set(0f, 0.8f);
+			uIElement.MaxWidth.Set(400f, 0f);
+			uIElement.Top.Set(220f, 0f);
+			uIElement.Height.Set(-520f, 1f);
+			uIElement.HAlign = 0.5f;
+
+			UIPanel uIPanel = new UIPanel();
+			uIPanel.Width.Set(0f, 1f);
+			uIPanel.Height.Set(-110f, 1f);
+			uIPanel.BackgroundColor = new Color(33, 43, 79) * 0.8f;
+			uIElement.Append(uIPanel);
+
+			containerPanel = uIPanel;
+			optionList = list;
+			optionList.Width.Set(-25f, 1f);
+			optionList.Height.Set(0f, 1f);
+			optionList.ListPadding = 5f;
+			uIPanel.Append(optionList);
+
+			UIScrollbar uIScrollbar = new UIScrollbar();
+			uIScrollbar.SetView(100f, 1000f);
+			uIScrollbar.Height.Set(0f, 1f);
+			uIScrollbar.HAlign = 1f;
+			uIPanel.Append(uIScrollbar);
+			optionList.SetScrollbar(uIScrollbar);
+
+			UITextPanel<string> uITextPanel = new UITextPanel<string>(title, 0.8f, large: true);
+			uITextPanel.HAlign = 0.5f;
+			uITextPanel.Top.Set(-35f, 0f);
+			uITextPanel.SetPadding(15f);
+			uITextPanel.BackgroundColor = new Color(73, 94, 171);
+			uIElement.Append(uITextPanel);
+
+			UITextPanel<LocalizedText> uITextPanel2 = new UITextPanel<LocalizedText>(Language.GetText("UI.Back"), 0.7f, large: true);
+			uITextPanel2.Width.Set(-10f, 1f);
+			uITextPanel2.Height.Set(50f, 0f);
+			uITextPanel2.VAlign = 1f;
+			uITextPanel2.Top.Set(-45f, 0f);
+			uITextPanel2.OnMouseOver += FadedMouseOver;
+			uITextPanel2.OnMouseOut += FadedMouseOut;
+			uITextPanel2.OnClick += cancelAction;
+			uIElement.Append(uITextPanel2);
+			backPanel = uITextPanel2;
+
+			base.Append(uIElement);
+		}
+
+		private void FadedMouseOver(UIMouseEvent evt, UIElement listeningElement)
+		{
+			Main.PlaySound(SoundID.MenuTick);
+			((UIPanel)evt.Target).BackgroundColor = new Color(73, 94, 171);
+		}
+
+		private void FadedMouseOut(UIMouseEvent evt, UIElement listeningElement)
+		{
+			((UIPanel)evt.Target).BackgroundColor = new Color(63, 82, 151) * 0.7f;
+		}
+
+		public override void OnActivate()
+		{
+			if (PlayerInput.UsingGamepadUI)
+			{
+				UILinkPointNavigator.ChangePoint(3000 + ((optionList.Count == 0) ? 1 : 2));
+			}
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			if (skipDraw)
+			{
+				skipDraw = false;
+				return;
+			}
+			base.Draw(spriteBatch);
+		}
+    }
+}
---
+++ UI/ListItem.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.Graphics;
+using Terraria.UI;
+
+namespace ExxoAvalonOrigins.UI
+{
+	public class ListItem : UIPanel
+	{
+
+		private Texture2D dividerTexture;
+		private Texture2D innerPanelTexture;
+		private UIImage worldIcon;
+		private Color textColor;
+
+		private string itemName;
+
+		public ListItem(string name, Texture2D tex, UIElement.MouseEvent submitEvent)
+		{
+			itemName = name;
+			LoadTextures();
+
+			base.OnClick += submitEvent;
+
+			worldIcon = new UIImage(tex);
+			worldIcon.Left.Set(4f, 0);
+			worldIcon.VAlign = 0.5f;
+			InitializeAppearance();
+			Append(worldIcon);
+		}
+
+		private void LoadTextures()
+		{
+			dividerTexture = TextureManager.Load("Images/UI/Divider");
+			innerPanelTexture = TextureManager.Load("Images/UI/InnerPanelBackground");
+		}
+
+		private void InitializeAppearance()
+		{
+			Height.Set(worldIcon.Height.Pixels + 16f, 0f);
+			Width.Set(0f, 1f);
+			SetPadding(6f);
+			BorderColor = new Color(89, 116, 213) * 0.7f;
+			textColor = Color.White * 0.9f;
+		}
+
+		public override void MouseOver(UIMouseEvent evt)
+		{
+			base.MouseOver(evt);
+			BackgroundColor = new Color(73, 94, 171);
+			BorderColor = new Color(89, 116, 213);
+			textColor = new Color(255, 215, 0);
+		}
+
+		public override void MouseOut(UIMouseEvent evt)
+		{
+			base.MouseOut(evt);
+			BackgroundColor = new Color(63, 82, 151) * 0.7f;
+			BorderColor = new Color(89, 116, 213) * 0.7f;
+			textColor = Color.White * 0.9f;
+		}
+
+		private void DrawPanel(SpriteBatch spriteBatch, Vector2 position, float width)
+		{
+			spriteBatch.Draw(innerPanelTexture, position, new Rectangle(0, 0, 8, innerPanelTexture.Height), Color.White);
+			spriteBatch.Draw(innerPanelTexture, new Vector2(position.X + 8f, position.Y), new Rectangle(8, 0, 8, innerPanelTexture.Height), Color.White, 0f, Vector2.Zero, new Vector2((width - 16f) / 8f, 1f), SpriteEffects.None, 0f);
+			spriteBatch.Draw(innerPanelTexture, new Vector2(position.X + width - 8f, position.Y), new Rectangle(16, 0, 8, innerPanelTexture.Height), Color.White);
+		}
+
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			base.DrawSelf(spriteBatch);
+			CalculatedStyle innerDimensions = GetInnerDimensions();
+			CalculatedStyle dimensions = worldIcon.GetDimensions();
+			float num = dimensions.X + dimensions.Width;
+			Vector2 vector = new Vector2(num + 6f, innerDimensions.Y + (innerDimensions.Height * 0.25f));
+			this.DrawPanel(spriteBatch, vector, innerDimensions.Width - dimensions.Width - 12f);
+			Utils.DrawBorderStringBig(spriteBatch, itemName, new Vector2(num + 12f, innerDimensions.Y + (innerDimensions.Height * 0.25f)), textColor, 0.45f);
+			spriteBatch.Draw(this.dividerTexture, new Vector2(num, innerDimensions.Y + (innerDimensions.Height * 0.65f)), null, Color.White, 0f, Vector2.Zero, new Vector2((GetDimensions().X + base.GetDimensions().Width - num) / 8f, 1f), SpriteEffects.None, 0f);
+		}
+	}
+}
