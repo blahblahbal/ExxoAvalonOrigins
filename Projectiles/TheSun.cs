ff8105f75475416038c5fe7ee31b891bb481bdcd
Brian Hansen <jabawakijadehawk@gmail.com>
2021-08-24 18:12:58 -0700

    More small fixes
    Scaled down the Stamina text above the Stamina display
    Slightly buffed Giga horn
    Made starlight cannon projectiles hit even when scaled up

--- Projectiles/TheSun.cs
+++ Projectiles/TheSun.cs
@@ -52,6 +52,18 @@ namespace ExxoAvalonOrigins.Projectiles
             projectile.rotation++;
             projectile.scale *= 1.002f;
             if (projectile.scale > 6f) projectile.scale = 6f;
+            var v = projectile.Center - new Vector2(projectile.width * projectile.scale / 2f, projectile.height * projectile.scale / 2f);
+            var wH = new Vector2(projectile.width * projectile.scale, projectile.height * projectile.scale);
+            var value2 = ExxoAvalonOrigins.NewRectVector2(v, wH);
+            var npc = Main.npc;
+            for (var num57 = 0; num57 < npc.Length; num57++)
+            {
+                var nPC = npc[num57];
+                if (nPC.active && !nPC.dontTakeDamage && !nPC.friendly && nPC.life >= 1 && nPC.getRect().Intersects(value2))
+                {
+                    if (projectile.ai[0] % 15 == 0) nPC.StrikeNPC(projectile.damage, projectile.knockBack, (nPC.Center.X < projectile.Center.X) ? -1 : 1, false, false);
+                }
+            }
             if (Main.rand.Next(2) == 0)
             {
                 int dust = Dust.NewDust(new Vector2((float)projectile.position.X, (float)projectile.position.Y), projectile.width, projectile.height, DustID.HallowedWeapons, 0, 0, 100, Color.White, 3.0f);
