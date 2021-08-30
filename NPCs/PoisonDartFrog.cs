a1f55d01229d91aa08c0bbd1131f5e1ad6b67e55
John Boyer <bojopro@gmail.com>
2021-08-31 01:18:26 +1000

    Fixed PoisonDartFrog not moving left or right on jump when colliding with wall and cleaned up the code

--- NPCs/PoisonDartFrog.cs
+++ NPCs/PoisonDartFrog.cs
@@ -1,85 +1,110 @@
 using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
-using Terraria.ModLoader;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace ExxoAvalonOrigins.NPCs
 {
-	public class PoisonDartFrog : ModNPC
-	{
-        bool jump = false;
+    public class PoisonDartFrog : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Poison Dart Frog");
+            Main.npcFrameCount[npc.type] = 3;
+        }
 
-		public override void SetStaticDefaults()
-		{
-			DisplayName.SetDefault("Poison Dart Frog");
-			Main.npcFrameCount[npc.type] = 3;
-		}
-
-		public override void SetDefaults()
-		{
-			npc.damage = 65;
-			npc.scale = 1f;
-			npc.lifeMax = 500;
-			npc.defense = 20;
-			npc.width = 22;
-			npc.aiStyle = -1;
-			npc.npcSlots = 1f;
-			npc.value = 0f;
-			npc.timeLeft = 750;
-			npc.height = 22;
-			npc.knockBackResist = 0.2f;
+        public override void SetDefaults()
+        {
+            npc.damage = 65;
+            npc.scale = 1f;
+            npc.lifeMax = 500;
+            npc.defense = 20;
+            npc.width = 22;
+            npc.aiStyle = -1;
+            npc.npcSlots = 1f;
+            npc.value = 0f;
+            npc.timeLeft = 750;
+            npc.height = 22;
+            npc.knockBackResist = 0.2f;
             npc.HitSound = SoundID.NPCHit1;
-	        npc.DeathSound = SoundID.NPCDeath1;
-			npc.buffImmune[BuffID.Poisoned] = true;
-			npc.buffImmune[BuffID.OnFire] = true;
-			npc.buffImmune[BuffID.Confused] = true;
-			npc.buffImmune[BuffID.CursedInferno] = true;
-		}
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.buffImmune[BuffID.Poisoned] = true;
+            npc.buffImmune[BuffID.OnFire] = true;
+            npc.buffImmune[BuffID.Confused] = true;
+            npc.buffImmune[BuffID.CursedInferno] = true;
+            AIInJump = true;
+        }
+
+        private const int AISlotFrame = 0;
+        private const int AISlotTimer = 1;
+        private const int AISlotInJump = 2;
+
+        public float AIFrame
+        {
+            get => npc.ai[AISlotFrame];
+            set => npc.ai[AISlotFrame] = value;
+        }
+
+        public float AITimer
+        {
+            get => npc.ai[AISlotTimer];
+            set => npc.ai[AISlotTimer] = value;
+        }
+
+        public bool AIInJump
+        {
+            get => npc.ai[AISlotInJump] == 1;
+            set => npc.ai[AISlotInJump] = value ? 1 : 0;
+        }
 
         public override void AI()
         {
-            npc.ai[1]++;
+            AITimer++;
             npc.TargetClosest(true);
             Player player = Main.player[npc.target];
             npc.spriteDirection = npc.direction;
-            if (npc.ai[1] == 1)
+
+            if (AITimer == 1)
             {
-                npc.ai[0] = 0;
+                AIFrame = 0;
             }
-            int jumpType = 0;
-            if (npc.ai[2] == 0)
+
+            // Ready to jump
+            if (AITimer >= 150)
             {
-                jumpType = Main.rand.Next(2);
-                npc.ai[2] = 1;
-            }
-            float verticalMovementSpeed = jumpType == 0 ? -5f : -8f;
-            float horizontalMovementSpeedFactor = 12f;
-            if (Vector2.Distance(npc.position, Main.player[npc.target].position) < 16 * 10)
-            {
-                verticalMovementSpeed = jumpType == 0 ? -7f : -10f;
-                horizontalMovementSpeedFactor = 5f;
-            }
-            if (npc.collideX)
-            {
-                verticalMovementSpeed *= 2;
-            }
-            if (npc.ai[1] >= 150)
-            {
-                npc.ai[0] = 1;
-                npc.velocity.Y = verticalMovementSpeed;
-                if (npc.ai[1] > 160)
+                int jumpType = Main.rand.Next(2);
+
+                float speedY = jumpType == 0 ? -5f : -8f;
+                float speedX = 12f;
+
+                if (Vector2.Distance(npc.position, Main.player[npc.target].position) < 16 * 10)
                 {
-                    npc.velocity.X += horizontalMovementSpeedFactor * npc.direction;
-                    npc.ai[1] = 0;
-                    npc.ai[2] = 0;
+                    speedY = jumpType == 0 ? -7f : -10f;
+                    speedX = 5f;
                 }
+
+                if (npc.collideX)
+                {
+                    speedY *= 2;
+                }
+
+                AIFrame = 1;
+                npc.velocity.Y = speedY;
+                npc.velocity.X += speedX * npc.direction;
+                AITimer = 0;
+                AIInJump = true;
             }
-            
+
+            if (AIInJump && npc.velocity.X == 0)
+            {
+                float speedX = 12f;
+                if (Vector2.Distance(npc.position, Main.player[npc.target].position) < 16 * 10)
+                {
+                    speedX = 5f;
+                }
+
+                npc.velocity.X += speedX * npc.direction;
+            }
 
             if (npc.collideY && npc.velocity.Y >= 0 && ExxoAvalonOriginsCollisions.SolidCollisionArma(npc.position, npc.width, npc.height))
             {
@@ -87,8 +112,9 @@ namespace ExxoAvalonOrigins.NPCs
                 if (npc.velocity.X > -0.5 && npc.velocity.X < 0.5)
                 {
                     npc.velocity.X = 0f;
-                    //npc.ai[0] = 0;
+                    //AIFrame = 0;
                 }
+                AIInJump = false;
             }
         }
 
@@ -100,13 +126,14 @@ namespace ExxoAvalonOrigins.NPCs
             }
             else
             {
-                if (npc.ai[0] == 0)
+                if (AIFrame == 0)
                 {
                     npc.frame.Y = frameHeight * 1;
                 }
                 else npc.frame.Y = frameHeight * 2;
             }
         }
+
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
             return spawnInfo.player.GetModPlayer<ExxoAvalonOriginsModPlayer>().zoneTropics && Main.hardMode ? 0.083f * ExxoAvalonOriginsGlobalNPC.endoSpawnRate : 0f;
