8a2504d2ed5e71cb89d26d9cf030a3af48422307
Lion8cake <69915693+Lion8cake@users.noreply.github.com>
2022-01-11 12:49:55 +1100

    Finished Costalqualite vanity
    finished the vanity

---
+++ Items/Vanity/CostalqualiteSnorkel.cs
@@ -0,0 +1,31 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExxoAvalonOrigins.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Head)]
+	class CostalqualiteSnorkel : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Costalqualite Snorkel");
+		}
+
+		public override void SetDefaults()
+		{
+			Rectangle dims = this.GetDims();
+			item.rare = ItemRarityID.Cyan;
+			item.width = dims.Width;
+			item.vanity = true;
+			item.value = Item.sellPrice(0, 20, 0, 0);
+			item.height = dims.Height;
+		}
+		
+		public override void DrawHair(ref bool drawHair, ref bool drawAltHair)
+	    {
+    		drawHair = (drawAltHair = true);
+	    }
+	}
+}
