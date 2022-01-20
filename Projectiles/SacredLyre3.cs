420207f038906eadc08b4a074f73ae40d90c5264
Brian Hansen <jabawakijadehawk@gmail.com>
2022-01-20 12:14:11 -0800

    Fix Sacred Lyre's shards being ranged instead of magic

--- Projectiles/SacredLyre3.cs
+++ Projectiles/SacredLyre3.cs
@@ -44,7 +44,9 @@ namespace ExxoAvalonOrigins.Projectiles
                 {
                     float num134 = -projectile.velocity.X * Main.rand.Next(40, 70) * 0.01f + Main.rand.Next(-20, 21) * 0.4f;
                     float num135 = -projectile.velocity.Y * Main.rand.Next(40, 70) * 0.01f + Main.rand.Next(-20, 21) * 0.4f;
-                    Projectile.NewProjectile(projectile.position.X + num134, projectile.position.Y + num135, num134, num135, ProjectileID.CrystalShard, (int)(projectile.damage * 0.8), 0f, projectile.owner, 0f, 0f);
+                    int proj = Projectile.NewProjectile(projectile.position.X + num134, projectile.position.Y + num135, num134, num135, ProjectileID.CrystalShard, (int)(projectile.damage * 0.8), 0f, projectile.owner, 0f, 0f);
+                    Main.projectile[proj].ranged = false;
+                    Main.projectile[proj].magic = true;
                 }
                 projectile.ai[0] += 1f;
                 if (projectile.ai[0] >= 9f)
